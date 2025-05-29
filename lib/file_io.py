f52240728c34d814ea84b8b0f7318580fa06eb83
Duncan Kariuki <duncankariukimugo@gmail.com>
2025-05-29 11:46:21 +0300

    finished lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
 def write_file(file_name, file_content):
+    file_name=f"{file_name}.txt"
+    with open(file_name, mode='w') as file:
+        file.write(file_content)
     pass
 
 def append_file(file_name, append_content):
+    file_name=f"{file_name}.txt"
+    with open(file_name,mode="a") as file:
+        file.write(append_content)
     pass
 
 def read_file(file_name):
+    file_name=f"{file_name}.txt"
+    with open(file_name,mode="r",encoding="utf-8") as file:
+        return file.read()
     pass
